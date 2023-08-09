9e181cdff69adeb1aaa8e5eceb59059bc9ffa9a0
Mahsa <mahskh08@gmail.com>
2023-08-09 12:03:40 +0430

    add third file

---
+++ js_exercises/exercise-three.js
@@ -0,0 +1,11 @@
+console.log(3 + 4); // Prints 7
+console.log(5 - 1); // Prints 4
+console.log(4 * 2); // Prints 8
+console.log(9 / 3); // Prints 3
+console.log(20 + 3.5); // prints my age
+// Subtract the corrent year
+console.log(2023 - 1969);
+// Devide 65 by 240
+console.log(65 / 240);
+console.log(0.2708 * 100);
+
