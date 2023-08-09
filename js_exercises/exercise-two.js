b47aafeb856dc79114de04df8dab3a5fbd0abcf9
Mahsa <mahskh08@gmail.com>
2023-08-09 11:54:39 +0430

    add second file

---
+++ js_exercises/exercise-two.js
@@ -0,0 +1,7 @@
+// Log the different data types to the console.
+console.log("JavaScript");
+console.log(2011);
+// Print the string to the console.
+console.log('Woohoo! I love to code! #codecademy');
+// Print the number to the console.
+console.log('20.49')
