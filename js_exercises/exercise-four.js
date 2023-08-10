8c2ec0cc6a54d4c467a2c94e146809f3e4a41bc0
Mahsa <mahskh08@gmail.com>
2023-08-10 21:23:38 +0430

    add fourth file

---
+++ js_exercises/exercise-four.js
@@ -0,0 +1,16 @@
+// String concatenation
+console.log('ya' + 'ha');
+console.log('wo' + 'ah');
+console.log('I love to '+ 'code.');
+// Prints 'I love to code.
+console.log('front' + 'space');
+// Prints 'front space'
+console.log('no' + 'space');
+// Prints 'nospace'
+console.log('middle' + ' ' + 'space');
+// Prints 'middle space'
+console.log('One' + ', ' + 'two' + ', ' + 'three!');
+// Prints 'One, two, three!
+console.log('Hello ' + 'World');
+console.log('add no space' + 'add');
+
