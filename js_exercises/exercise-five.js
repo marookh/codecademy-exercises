126f55db50f3cb63d84ed4c2c53862dfce961ddc
Mahsa <mahskh08@gmail.com>
2023-08-10 21:30:17 +0430

    add file

---
+++ js_exercises/exercise-five.js
@@ -0,0 +1,12 @@
+// The beginning line
+console.log('Hello'.length); // Prints 5
+console.log('Teaching the world how to code'.length);
+// Prints the number of characters within the stirng
+console.log('Wowww! learning how to code is amazing...');
+console.log('I love to code!');
+console.log('Change your life with recitation of the Holy Quran'.length);
+console.log("Don't worry about the future, Allah will make every thing fine");
+console.log('Be positive'.length);
+// Print the length of the following string.
+console.log('It will give you a feel of hope and happeness to work hard and see the results....'.length);
+
