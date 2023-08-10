256802d6da868fb111f3a37d9710e591670ade1e
Mahsa <mahskh08@gmail.com>
2023-08-10 21:35:40 +0430

    add seventh file

---
+++ js_exercises/exercise-seven.js
@@ -0,0 +1,16 @@
+// First line
+// Prents the decimal number between 0 and 1.
+console.log(Math.random());
+// Prints the number between 0 and 20.
+// log a random number with Math.random then multiply it by 100.
+console.log(Math.random() * 20);
+// Use Math.floor() to make the output a whole   number.
+// Prints a whole number between 0 and 50.
+console.log(Math.floor(Math.random() * 50));
+// Use the Math.ceil to return the smallest integer greater than or equal to a decimal number.
+// Log the answer to the console.
+console.log(Math.ceil(43.8));
+// Find a method on the built-in Number object that checks if a number is an integer.
+// Put the number 2017 in the parentheses of the method and use consol.log() to print the result.
+console.log(Number.isInteger(2017));
+
