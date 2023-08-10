6e576e88381c0fadc7a098e074a1884c982ffd0c
Mahsa <mahskh08@gmail.com>
2023-08-10 21:33:06 +0430

    add sixth file

---
+++ js_exercises/exercise-six.js
@@ -0,0 +1,19 @@
+// String methods
+console.log("hello".toUpperCase()); // Prints 'HELLO'
+console.log('Hey'.startsWith('H')); // Prints true
+// Use the .toUpperCase() method to log the string 'Codecademy' to the console in all capital letters.
+console.log('Codecademy'.toUpperCase());
+// Remove whitespace
+console.log('  remote whitespace  '.trim());
+// Engrave my words on the rock!!!
+console.log('Hello, world. I am an afghan girl, I have goals, ambitions, hopes, and talent. I want to change my life and myself to the better version of it.');
+// Change to the uppercasse..
+console.log('rock onn'.toUpperCase());
+// Trim the white spaces
+console.log('practice   '.trim());
+// Check the starting letter
+console.log('save your time'.startsWith('s'));
+console.log('NICE'.toLowerCase());
+console.log('AFGHANISTAN'.toLowerCase());
+console.log('new york'.toUpperCase());
+
