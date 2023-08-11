b0a448cd6aca96d4daf36472e2a903c63c9ab20c
Mahsa <mahskh08@gmail.com>
2023-08-11 05:45:18 +0430

    add the file

---
+++ js_exercises/exercise-eight.js
@@ -0,0 +1,13 @@
+var look = 'nice'
+var age = 55
+var isCorrect = true
+var isRight = false 
+var aVariable = undefined
+var nothing = null 
+// Declare a variable
+var favoriteFood = "pizza";
+var numOfSlices = 8;
+console.log(numOfSlices);
+console.log(favoriteFood);
+numOfSlices = 3;
+console.log(numOfSlices);
