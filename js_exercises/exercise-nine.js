78d514e55bb74d0855a27b903c3c169039a7e31d
Mahsa <mahskh08@gmail.com>
2023-08-12 20:09:59 +0430

    add file

---
+++ js_exercises/exercise-nine.js
@@ -0,0 +1,14 @@
+
+// console.log(chicken);
+let bar = 'ICE CREAM';
+console.log(bar);
+let chicken = '22 eggs';
+console.log(chicken);
+// print the value
+let variable = 'codecademy'
+console.log(variable);
+console.log(variable.toUpperCase());
+console.log (chicken.startsWith('2'));
+console.log(bar.toLowerCase());
+let aBicycle = '$100'
+console.log(aBicycle);
