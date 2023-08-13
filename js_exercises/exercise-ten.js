3b5e3c36f91bc886366ef94958cbda610c82fca6
Mahsa <mahskh08@gmail.com>
2023-08-13 12:34:19 +0430

    add file

---
+++ js_exercises/exercise-ten.js
@@ -0,0 +1,11 @@
+// Opening line
+// Declare some variables with const keyword
+const myName = 'Mahsa';
+console.log(myName);
+// Assigning a value that won't change later
+const inchPerMeter = 100;
+const entree = 'Encheladas';
+console.log(entree);
+const thirtyFourProvinces = 34;
+console.log(thirtyFourProvinces)
+
