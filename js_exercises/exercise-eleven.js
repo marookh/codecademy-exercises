830b606ca86bd6ff504dfc65f2517371ae6c76c6
Mahsa <mahskh08@gmail.com>
2023-08-13 12:40:57 +0430

    add some changes to the file

---
+++ js_exercises/exercise-eleven.js
@@ -0,0 +1,24 @@
+// Assigning value to variable
+let number = 342;
+let zero = 0
+let doors = 32;
+let ranking = 5;
+number = number + 2
+console.log(number);
+number += 2;
+console.log(number);
+zero = zero - 3;
+// Log the value
+console.log(zero);
+// Multiply the variable
+doors = doors * 4;
+console.log(doors);
+// Reassigning the variable
+ranking = ranking / 5;
+console.log(ranking);
+number += 4;
+console.log(number);
+zero -= 2;
+doors *= 2;
+ranking /= 1;
+console.log(number + zero + doors + ranking);
