9ee83a697dc1c4c6391ee713e566d23ff3810411
Mahsa <mahskh08@gmail.com>
2023-08-13 12:46:56 +0430

    add changes to the file

---
+++ js_exercises/exercise-twelve.js
@@ -0,0 +1,17 @@
+let gainedDollar = 3;
+gainedDollar++;
+let lostDollar = 50;
+lostDollar--;
+console.log(gainedDollar, lostDollar);
+// Assign the variable
+let number = 33;
+// Increase the value
+number++;
+let string = 23;
+// Decrease the value
+string--;
+console.log(number, string);
+
+let numOfCustomers = 20;
+numOfCustomers++;
+console.log(numOfCustomers);
