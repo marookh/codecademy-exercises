7d8f1927b4204e168d7116541851a1762c4b68c9
Mahsa <mahskh08@gmail.com>
2023-08-16 21:42:57 +0430

    exercise with ansers

---
+++ js_exercises/exercise-fiveteen.js
@@ -0,0 +1,12 @@
+// Playing around with typeof operator.
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+let myVariable = 'What is the type of this value?';
+console.log(typeof myVariable);
+let typeOfData = 35;
+let typeOfIt = 'console.log';
+// Check the type of the value.
+console.log(typeof typeOfData);
+console.log(typeof typeOfIt);
