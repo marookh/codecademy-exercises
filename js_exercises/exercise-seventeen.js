f35fba64753170b00af4de3347f02f13514ea73f
Mahsa <mahskh08@gmail.com>
2023-08-16 21:52:11 +0430

    add dog-years project

---
+++ js_exercises/exercise-seventeen.js
@@ -0,0 +1,15 @@
+// my age
+const myAge = 21;
+// dog's earlyyears
+let earlyYears = 2;
+earlyYears *= 10.5; 
+// subtract 2 from myage variable
+let laterYears = myAge - 2;
+// multiply lateryear by 4
+laterYears = 4;
+// add two variables
+let myAgeInDogYears = laterYears + earlyYears; 
+// my name to lower case
+let myName = 'CHRIS'.toLowerCase();
+console.log(`My name is ${myName} and my age is ${myAge} years old in human years which is ${myAgeInDogYears} year old in dog years.`);
+
