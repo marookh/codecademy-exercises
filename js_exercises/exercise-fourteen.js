5b6f5c8f8c3986f743e10f3aba6cc9b26f3a7170
Mahsa <mahskh08@gmail.com>
2023-08-16 21:39:33 +0430

    add some changes to the file

---
+++ js_exercises/exercise-fourteen.js
@@ -0,0 +1,15 @@
+const myName = 'Mahsa';
+const myCity = 'Dobai';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+const myPet = 'jojoje';
+console.log(`I own a pet ${myPet}. ${myCity}`)
+console.log(`My name is {myName.}`);
+let aString = `This is a valid string`;
+console.log(aString);
+let backTick = `\` \``
+let singleString = `
+1. A single set of back ticks (${backTick}) to enclose the entire line. 
+2. Direct interpolation of a variable, in place in the string.
+3. The ability to write multiple lines in a single string.
+`
+console.log(singleString)
