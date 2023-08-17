a7ca17bc2be30c16827238ec2256b4e7312a272d
Mahsa <mahskh08@gmail.com>
2023-08-17 16:59:50 +0430

    add statements

---
+++ js_exercises/exercise-nineteen.js
@@ -0,0 +1,14 @@
+let sale = true;
+sale = false;
+if(true) { // if...else statements
+  console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+// Checking the condition
+var next = false;
+if (next) {
+  console.log('The code in this block will not run.');
+} else {
+  console.log('But the code in this block will!');
+}
