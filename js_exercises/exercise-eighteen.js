f4f053051f44df622c2eb05f831562016d9ab1b8
Mahsa <mahskh08@gmail.com>
2023-08-17 16:54:53 +0430

    add some changes

---
+++ js_exercises/exercise-eighteen.js
@@ -0,0 +1,11 @@
+let sale = true;
+if (sale) {
+  console.log('Time to buy!');
+}
+let condition = true;
+if (condition) {
+  console.log('This is for practice.');
+}
+if (true) {
+  console.log('This message will print!')
+} // Prints: This message will print!
