910f7c879c90041e041f5e16d0822c4b3fbdcd9c
Mahsa <mahskh08@gmail.com>
2023-08-18 17:00:53 +0430

    add the exercise

---
+++ js_exercises/exercise-twentyone.js
@@ -0,0 +1,47 @@
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (!(mood === 'sleepy' && tirednessLevel > 8)) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet')
+}
+// Assigning a variable
+let stopLight = 'red';
+let pedestrians = 0;
+// Using the if...else statement
+if(stopLight === 'green' && pedestrians === 0) {
+  console.log('Go!');
+} else {
+  console.log('Stop');
+}
+// Using the if...else statment 
+let day = 'Saturday'
+let today = 'Sunday'
+if (day === 'Saturday' || day === 'Sunday') {
+  console.log('Enjoy the weekend!');
+} else {
+  console.log('Do some work.');
+} // Prints 'Enjoy the weeken'
+
+let excited = true;
+console.log(!excited); // Prints false
+let sleepy = false;
+console.log(!sleepy); // Prints true
+let days = 'practice';
+if (mood === 'practice' || mood === 'job') {
+  console.log('This is practice time.');
+} else {
+  console.log('This is not practice time.')
+};
+let color = 'red';
+if (color === 'red' && color === 'red') {
+  console.log('this is my favorite color.');
+} else {
+  console.log('this is not my favorite color.');
+}
+
+let execute = true;
+console.log(!excited);
+let sleep = false;
+console.log(`${sleep} is ${!true}`);
+
