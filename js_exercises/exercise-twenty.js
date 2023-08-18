41b6d4c1ef1999dcf62350cb24b6e1362cbe9e5c
Mahsa <mahskh08@gmail.com>
2023-08-18 16:59:06 +0430

    add the exercise

---
+++ js_exercises/exercise-twenty.js
@@ -0,0 +1,19 @@
+let hungerLevel = 7;
+if (hungerLevel <= 6) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!')
+}
+let tirednessLevel = 10;
+if (tirednessLevel >= 10) {
+  console.log('Time to sleep.');
+} else {
+   console.log('We can stay more awake.');
+}
+let grades = 98;
+if (grades >= 95) {
+  console.log('congratulation you are passed');
+} else {
+  console.log('you are in lower ranking');
+}
+
