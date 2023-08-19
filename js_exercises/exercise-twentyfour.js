d0d2c916feb20ce93b4b2df4362205a53f42f868
Mahsa <mahskh08@gmail.com>
2023-08-19 19:32:47 +0430

    add some changes to the file

---
+++ js_exercises/exercise-twentyfour.js
@@ -0,0 +1,15 @@
+let isLocked = true;
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+let isCorrect = true;
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+let isMorning = 00;
+isMorning ? console.log('Good morning!') :
+console.log('You have more time to sleep!');
+let isNightTime = true;
+isNightTime ? console.log('Turn on the lights') :
+console.log('Turn off the lights');
+let isANumber = 0;
+isANumber ? console.log('this is my favorite number') : console.log('This is not my favorite number');
