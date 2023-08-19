37a36445c683970a434a904057f5d4dce1f28de2
Mahsa <mahskh08@gmail.com>
2023-08-19 19:34:30 +0430

    add the file with exercises

---
+++ js_exercises/exercise-twentyfive.js
@@ -0,0 +1,36 @@
+let season = 'fall';
+// Create a program that keeps track of the way the environment changes with the seasons.
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if (season === 'winter') { // Checks if it evaluates to true or false.
+  console.log('It\s winter! Everything is covered in snow.'); 
+} else if (season === 'fall') {
+  console.log('It\s fall! Leaves are falling!');
+} else {
+  console.log('Invalid season.');
+}
+
+// Assigning a variable
+let day = 'Sunday'
+if (day === 'saturdy') {
+  cosnole.log('Today is weekend');
+} else if (day === 'monday') {
+  console.log('start works');
+} else if (day === 'Sunday'){
+  console.log('Today we go to the party.');
+} else {
+  console.log('Today is wednesday');
+}
+
+// Using different conditional statements
+let stopLight = 'yellow';
+if (stopLight === 'red') {
+  console.log('Stop!');
+} else if (stopLight === 'yellow') {
+  console.log('Slow down');
+} else if (stopLight === 'green') {
+  console.log('Go!');
+} else {
+  console.log('Caution, unknown!');
+}
+
