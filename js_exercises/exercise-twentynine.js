091fc379d54cd9b5cd8ec69fe5b581027d4040f9
Mahsa <mahskh08@gmail.com>
2023-08-21 18:12:49 +0430

    add some changes

---
+++ js_exercises/exercise-twentynine.js
@@ -0,0 +1,15 @@
+function myFunction(parameter) {
+   console.log(parameter);
+}
+myFunction('Looking nice!')
+function greetWorld() {
+  console.log('Hello, World!');
+}
+greetWorld();
+
+function tellMeAJoke() {
+  console.log("Google Assistant: “One joke, coming up! What is a sea monster's favorite snack? Ships and dip.” 🛳")
+}
+
+// Calling a function 
+tellMeAJoke()
