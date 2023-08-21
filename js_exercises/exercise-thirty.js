09eb58c27eb02dfca462fac9c5271faa66cc7fab
Mahsa <mahskh08@gmail.com>
2023-08-21 18:25:30 +0430

    adds the exercise

---
+++ js_exercises/exercise-thirty.js
@@ -0,0 +1,19 @@
+function getReminder() {
+  console.log("Sleepyhead! Rise and Shine!");
+}
+getReminder();
+// Declaring a function
+function saySomethingPositive() {
+  console.log('You are perfect just the way you are., You are an incredible human.')
+}
+saySomethingPositive();
+function myFavoritePlaces () {
+  let country = 'Canada'
+  if (country === 'China') {
+    console.log('I love to visit ' + country + '.');
+  } else {
+    console.log('I love to live in ' + country + '.')
+  }
+}
+
+myFavoritePlaces();
