7242f56c89e62c6745d874d43b2caebaf34e1600
Mahsa <mahskh08@gmail.com>
2023-08-22 17:00:06 +0430

    add the exercise

---
+++ js_exercises/exercise-thirtytwo.js
@@ -0,0 +1,13 @@
+function sayThanks(name) {
+  console.log(`Thank you for your purchase ${name}! We appreciate your business.`);
+}
+sayThanks('Aziza');
+function yourAge(age) {
+  console.log(`you are ${age} year old!`);
+}
+yourAge(34);
+function parameters(width, height) {
+  let area = width * height;
+  return area;
+}
+console.log(parameters(5,6));
