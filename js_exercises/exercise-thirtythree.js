c6ee7d82fea2ed62a498e91a2c033e5aa954fcc6
Mahsa <mahskh08@gmail.com>
2023-08-22 17:14:51 +0430

    add the exercise

---
+++ js_exercises/exercise-thirtythree.js
@@ -0,0 +1,13 @@
+function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs') {
+  console.log(`Remember to buy ${item1}`);
+  console.log(`Remember to buy ${item2}`);
+  console.log(`Remember to buy ${item3}`);
+}
+makeShoppingList('curtains', 'pen', 'Iron');
+function makeList(name = 'Marcus', age = '20', height = '185 cm') {
+  console.log(`Hi, I am ${name}.`);
+  console.log(`I am ${age} years old!`);
+  console.log(`My height is ${height}.`);
+}
+makeList();
+
