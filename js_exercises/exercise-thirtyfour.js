8de6d5ba147031aadb2a36f64f1d4a53d33869f7
Mahsa <mahskh08@gmail.com>
2023-08-22 17:17:37 +0430

    add the exercise

---
+++ js_exercises/exercise-thirtyfour.js
@@ -0,0 +1,33 @@
+function rectangleArea(width, height) {
+  let are = width * height;
+}
+console.log(rectangleArea(3,4))
+function monitorCount(rows, columns) {
+ return rows * columns;
+}
+const numOfMonitors = monitorCount(5, 4);
+console.log(numOfMonitors); 
+function rectangleArea(width, height) {
+  let area = width * height;
+  return area;
+}
+console.log(rectangleArea(5, 7)) // Prints 35
+ function rectingleArea(width, height) {
+   let area = width * height;
+   return area;
+ }
+console.log(rectingleArea(20,30)); // Prints 600
+
+
+
+
+
+
+
+
+
+
+
+
+
+
