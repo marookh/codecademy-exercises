41ce1669cf54a932d0379d5f14de00202d8d034e
Mahsa <mahskh08@gmail.com>
2023-08-23 12:41:19 +0430

    add the exercise

---
+++ js_exercises/exercise-thirtysix.js
@@ -0,0 +1,19 @@
+
+const plantNeedsWater = function(day) {
+  if (day === 'sunday') {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(plantNeedsWater('sunday'));
+const shoppingDay = function (day) {
+  if (day === 'thursday') {
+    day.toLowerCase
+    return console.log(`It's ${day} let's go shopping!`)
+  } else {
+    return console.log('Do some work!')
+  }
+};
+
+shoppingDay('thursday');
