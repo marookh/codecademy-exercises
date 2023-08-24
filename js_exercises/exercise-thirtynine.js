c5396d3112856a1795f529a632251a496dd78923
Mahsa <mahskh08@gmail.com>
2023-08-24 10:45:44 +0430

    add the exercise

---
+++ js_exercises/exercise-thirtynine.js
@@ -0,0 +1,7 @@
+let city = 'New York City';
+function logCountrySkyLine() {
+  let skyscraper = 'Empire State Building';
+  return 'The stars over the ' + skyscraper + ' in ' + city + '.';
+}
+console.log(logCountrySkyLine())
+
