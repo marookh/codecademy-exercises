4e01ec11027d96aa09191e8d6f3a900b7f4d4a29
Mahsa <mahskh08@gmail.com>
2023-08-27 16:20:55 +0430

    add the exercise

---
+++ js_exercises/exercise-forty-four.js
@@ -0,0 +1,13 @@
+const array = [385, 343, true, false, 'aziza'];
+function arrayFunction(arr) {
+  return arr.shift();
+  
+}
+arrayFunction(array);
+console.log(array);
+arrayFunction(array);
+array.slice(0, 2);
+console.log(array); 
+const array1 = [true, false, 'aziza', 332];
+array1.slice(0, 3)
+console.log(array1)
