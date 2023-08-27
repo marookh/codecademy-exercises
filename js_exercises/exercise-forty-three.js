e2e1d58250eac00e35a9ea0a18f2dd28f545f776
Mahsa <mahskh08@gmail.com>
2023-08-27 16:19:33 +0430

    add the exercise

---
+++ js_exercises/exercise-forty-three.js
@@ -0,0 +1,8 @@
+let newArray = [['aziza', 'ayoub'],[true, false],[34, 54]];
+console.log(newArray[0][0]);
+console.log(newArray[1][0]);
+const nestArr = [[1], [2, 3]];
+console.log(nestArr[1][1]);
+console.log(nestArr[1][0])
+const azizaArray = [['a', 'z'], ['i', 'z'], ['a']];
+console.log(azizaArray[0][1]);
