61547ff569c3a1f81623a3b4521b476f0f37accf
Mahsa <mahskh08@gmail.com>
2023-08-28 16:42:26 +0430

    add the exercise

---
+++ js_exercises/exercise-forty-eight.js
@@ -0,0 +1,12 @@
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+
+seasons[3] = 'Autumn';
+console.log(seasons);
+// Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+const fruits = ['Apple', 'Pineapple', 'Mango', 'Grape', 'Banana', 'Kiwi']
+console.log(fruits);
+fruits[2] = 'Blueberries';
+console.log(fruits);
