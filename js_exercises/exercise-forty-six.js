632911229a85112dff6e43695b3e8db89cbf6b83
Mahsa <mahskh08@gmail.com>
2023-08-28 16:35:23 +0430

    add the exercise

---
+++ js_exercises/exercise-forty-six.js
@@ -0,0 +1,14 @@
+const hobbies = ['popcorn', 'watching movie', 'plying game']
+
+const tools = ['array', 'variable', 'function', 'scope', 'object'];
+let dataTypes = ['strings', 'boolens', 'number', 'null'];
+console.log(tools[1]);
+dataTypes[2] = 'object';
+console.log(dataTypes);
+console.log(tools);
+
+const countries = ['Afghanista', 'Pakistan', 'Iran', 'Ozbekestan', 'Tajikstan', 'China', 'Turkey'];
+const cities = ['Kabul', 'Islam Abad', 'Tehran', 'Dushanba', 'Bejing', 'Istanbul', 'Samarqand'];
+console.log(cities);
+console.log(countries);
+
