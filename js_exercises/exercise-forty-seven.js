ba5349f880cd90c307efa5d06138006025514ff4
Mahsa <mahskh08@gmail.com>
2023-08-28 16:38:06 +0430

    adds the exercise with answers

---
+++ js_exercises/exercise-forty-seven.js
@@ -0,0 +1,5 @@
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+let listItem = famousSayings[0]
+console.log(listItem); // Prints: Fortune favors the brave.
+console.log(famousSayings[2]);// Prints: Where there is love, there is life.
+console.log(famousSayings[3]); // Prints undefined
