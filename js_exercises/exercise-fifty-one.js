6e6163e36ced7a0133c76f99e8458f102c9f6fee
Mahsa <mahskh08@gmail.com>
2023-08-29 16:56:00 +0430

    adds some changes to the previous file

---
+++ js_exercises/exercise-fifty-one.js
@@ -0,0 +1,35 @@
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+chores.pop()
+console.log(chores);
+// Create an array
+const rooms = ['Living room', 'Bedroom', 'kitchen', 'Bathroom', 'Hall', 'Dinning room'];
+// Delete the last element using .pop() method
+rooms.pop()
+console.log(rooms); // Print the array
+console.log(rooms.length); // Explore the length of array's elements
+rooms.push('Basement', 'Dinnig room'); // Add two elements to the end of array
+console.log(rooms);
+console.log(rooms.length);
+console.log(rooms.pop());
+console.log(rooms);
+// Create an array usin let keyword
+let browsers = ['firefox', 'brave', 'opera', 'ark', 'chrome', 'dolphin', 'mozilla'];
+console.log(browsers);
+let removed = browsers.pop()
+console.log(browsers);
+console.log(removed);
+// Playing around with arrays
+// An array containing numbers
+const numberArray = [0, 1, 2, 3, 4];
+// An array containing different data types
+const mixarray = [false, 3, 'string'];
+const fruit = ['Orange', 'Banana', 'Mango'];
+console.log(fruit);
+console.log(fruit[0]);
+fruit[1] = 'Melon' // Melon
+console.log(fruit);
+fruit[4] = 'Apple'; // A valid assignment
+console.log(fruit);
+console.log(fruit[3]);
+// Create a nested array
+const food = ['orange', 'banana'];
