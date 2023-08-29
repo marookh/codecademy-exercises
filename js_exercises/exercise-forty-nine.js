f52d896ff32f433d10d475d995ac9b817886d83a
Mahsa <mahskh08@gmail.com>
2023-08-29 16:50:28 +0430

    add some changes to the file

---
+++ js_exercises/exercise-forty-nine.js
@@ -0,0 +1,28 @@
+const objectives = ['Learn a new language', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
+const goals = ['Learn English', 'Learn JavaScript','Travel to Dobai'];
+console.log(goals.length);
+let colors = ['blue', 'yellow', 'green', 'white','black', 'cream', 'brown'];
+console.log(colors.length);
+// Output: 7
+let books = ['math', 'chemistry', 'dari', 'history', 'phesics', 'computer', 'art', 'English'];
+console.log(books.length);
+const utensils = ['Fork', 'Spoon', 'HotPot', 'Glasses', 'Sink', 'refregerator', 'Stove'];
+console.log(utensils.length);
+const hardwares = ['Wire', 'Charger', 'Monitor', 'Mouse', 'HardDrive', 'Keyboard'];
+console.log(hardwares.length);
+const tabs = ['Darya.net', 'Codecademy.com', 'Typing.com', 'Netflix.com', 'GitHub.com', 'Google Drive', 'Google Docs', 'Google Translate'];
+console.log(tabs.length);
+const toDoList = ['Wake up early', 'Wash up', 'Have healthy breakfast', 'Get ready', 'Do choreses', 'Take a study session', 'Have lunch', 'Take second study session', 'Walk around', 'Take online class', 'Take another study session', 'Go to bed'];
+console.log(toDoList.length);
+let makeAParty = ['Buy a cake', 'Buy some ballons', 'Have a saloon ready for the party', 'Invite friends'];
+console.log(makeAParty.length);
+console.log(makeAParty[0].length);
+console.log(toDoList[4].length);
+function doList() {
+  console.log(toDoList);
+}
+doList()
+toDoList.length;
+console.log(toDoList.length);
+
