16915b7669d79b519095bf1fb53d15d496db4d11
Mahsa <mahskh08@gmail.com>
2023-08-31 21:52:28 +0430

    add the file

---
+++ js_exercises/exercise-fifty-four.js
@@ -0,0 +1,11 @@
+for (let j = 20; j >= 0; j--) {
+  console.log(j);
+}
+
+for (let a = 50; a > 0; a--) {
+  console.log('I love to code!')
+};
+
+for (let i = 22; i > 0; i--) {
+  console.log('Happy coding!');
+};
