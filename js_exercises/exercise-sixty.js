3d5a926a503bca2ec80c2dbecdd9133f2c333b73
Mahsa <mahskh08@gmail.com>
2023-09-02 16:41:02 +0430

    add some practice

---
+++ js_exercises/exercise-sixty.js
@@ -0,0 +1,7 @@
+// A review of exercise
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+let secondArray = [45, 34,]
+for (let i = 0; i < groceryList.length; i++) {
+  console.log(groceryList[i]);
+}
+
