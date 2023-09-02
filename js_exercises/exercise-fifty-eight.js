86f6e5ff6cd6703f44da56a990ac3f8328cd67bd
Mahsa <mahskh08@gmail.com>
2023-09-02 16:37:02 +0430

    add the exercise

---
+++ js_exercises/exercise-fifty-eight.js
@@ -0,0 +1,25 @@
+// cake-baking process
+let cupsOfSugarNeeded = 0;
+let cupsAdded = 0;
+do {
+  cupsAdded++;
+} while (cupsAdded < cupsOfSugarNeeded)
+console.log(cupsAdded);
+// Add students' names to the list of qualifiers
+let namesOfStudents = ['Mahsa', 'Asma', 'Nagina'];
+let addedStudents = 0;
+let i = 0;
+do {
+  addedStudents = namesOfStudents[i]
+  i++;
+  console.log(addedStudents);
+} while (addedStudents < namesOfStudents);
+
+// Add some cups of water to the boiling rice
+
+let cupsOfWater = 5;
+let addedCups = 0;
+do {
+  addedCups++;
+  console.log(addedCups);
+} while (addedCups < cupsOfWater);
