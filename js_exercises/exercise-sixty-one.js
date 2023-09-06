b4552ea2b927bc447a68f1d8de2a3b41ac2f5f92
Mahsa <mahskh08@gmail.com>
2023-09-06 19:10:35 +0430

    add some changes to the file

--- js_exercises/exercise-sixty-one.js
+++ js_exercises/exercise-sixty-one.js
@@ -6,3 +6,32 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
   }
 };
 
+onst anotherFunc = () => {
+  return 'I\'m being invoked by the higher-order function!';
+}
+
+higherOrderFunc(anotherFunc);
+console.log(higherOrderFunc(anotherFunc));
+
+higherOrderFunc(() => {
+  for (let i = 0; i <= 10; i++){
+    console.log(i);
+  }
+});
+
+console.log(higherOrderFunc(addTwo));
+const countToZero = (num) => {
+  for (let i = num; i <= 0; i--) {
+    console.log(i);
+  }
+}
+
+console.log(higherOrderFunc(countToZero));
+checkConsistentOutput(countToZero, 22);
+const array1 = ['aziza'];
+const aFunction = num => {
+  return num === 'aziza';
+}
+const aziza = aFunction(array1)
+console.log(aziza);
+
