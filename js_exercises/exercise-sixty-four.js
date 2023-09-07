40d7e8d67e9bda60cce3c937fbf6e0a402e11d2d
Mahsa <mahskh08@gmail.com>
2023-09-07 20:57:51 +0430

    add some changes

---
+++ js_exercises/exercise-sixty-four.js
@@ -0,0 +1,29 @@
+const fruitsList = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits 
+fruitsList.forEach(fruitList => {
+  console.log('I want to eat a ' + fruitList + '.')
+});
+
+fruitsList.forEach(fruitsList => {
+  console.log('I like this! ' + fruitsList);
+});
+function printGrocery(element){
+  console.log(element);
+}
+
+fruitsList.forEach(printGrocery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
