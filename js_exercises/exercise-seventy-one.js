18b7156d4c29f6da7572bf8dc1a0564cdcddcc80
Mahsa <mahskh08@gmail.com>
2023-09-11 15:00:29 +0430

    add the exercise

---
+++ js_exercises/exercise-seventy-one.js
@@ -0,0 +1,45 @@
+// Write your fasterShip object literal below
+let fasterShip = {
+  'Fuel Type': 'Turbo Fuel', color: 'silver'
+};
+// Creating some objects
+const banana = {
+  name: 'Banana',
+  color: 'yellow',
+   size: '5 inch', 
+   peel: true, 
+  'Vitamin Type': 'B6'
+};
+console.log(banana);
+let myHouse = {
+  size: 'Two stories', 
+  colors: 'White and blue',
+  type: 'Duplex',
+  rooms: 8,
+  bathrooms: 4, 
+  basement: true,
+  yard: false, 
+  'Water Pool': false
+};
+console.log(myHouse);
+let  ingredents = {
+  'Cocoa Powder': '1 cup', 
+  sugar: '2 cups', 
+  'Baking powder': '1 1/2 teaspoon', 
+  eggs: '3 large', 
+  salt: '1 teaspoon', 
+  'Baking suda': '1 teaspoon', 
+  'all pupose flour': '2 1/2 cups', 
+  Oil: '3/4 cups', 
+  'Vanila extract': '2 teaspoon', 
+  'soure cream': '1/2 cup', 
+  'Cooking spray': 'as needed'
+};
+let bakeACake = {
+  'Cake Type': 'Chocolate cake', 
+  materials: ingredents,
+};
+console.log(bakeACake);
+
+
+
