fafbe80d0463badfdf685220de4936e14f650305
Mahsa <mahskh08@gmail.com>
2023-09-13 10:41:55 +0430

    add the exercise

---
+++ js_exercises/exercise-seventy-eight.js
@@ -0,0 +1,35 @@
+// Looping through opjects
+let spaceship = {
+    crew: {
+    captain: { 
+        name: 'Lily', 
+        degree: 'Computer Engineering', 
+        cheerTeam() { console.log('You got this!') } 
+        },
+    'chief officer': { 
+        name: 'Dan', 
+        degree: 'Aerospace Engineering', 
+        agree() { console.log('I agree, captain!') } 
+        },
+    medic: { 
+        name: 'Clementine', 
+        degree: 'Physics', 
+        announce() {
+          console.log('Jets on!')
+        }
+      },
+    translator: {
+        name: 'Shauna', 
+        degree: 'Conservation Science', 
+        powerFuel() { console.log('The tank is full!') } 
+        }
+    }
+}; 
+
+
+for (let checkmembers in spaceship.crew) {
+  console.log(`${checkmembers}: ${spaceship.crew [checkmembers].name}`)
+}
+for (let checkdegree in spaceship.crew) {
+  console.log(`${checkdegree} ${spaceship.crew[checkdegree].degree}`)
+}
